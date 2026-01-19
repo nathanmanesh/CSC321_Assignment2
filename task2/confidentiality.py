6a5e8ad9a6feea7238bdbc13fb616b1b02436b01
nathanmanesh <nathanmanesh2@gmail.com>
2026-01-18 16:26:05 -0800

    nathan's version of verify()

--- task2/confidentiality.py
+++ task2/confidentiality.py
@@ -4,7 +4,9 @@ from task1.CBC import *
 from task1.EBC import *
 
 from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
 
+from urllib.parse import unquote
 from urllib.parse import quote
 
 AES_BLOCK_SIZE = 16  # bytes (128 bits) for AES
@@ -32,10 +34,43 @@ def submit(string: str) -> bytes:
     ciphertext = ecb_encrypt(padded, KEY)
     return ciphertext
 
+def pkcs7_unpad(data: bytes, block_size: int = AES_BLOCK_SIZE) -> bytes:
 
-def verify():
-    pass
+    if len(data) == 0 or (len(data) % block_size) != 0:
+        raise ValueError("invalid padded length")
+    
+    pad_len = data[-1]
+    if pad_len < 1 or pad_len > block_size:
+        raise ValueError("invalid PKCS#7 padding bytes")
+    
+    return data[:-pad_len]
 
+
+def verify(ciphertext: bytes) -> bool:
+
+    try:
+
+        # create a cipher object with the same key used for encryption
+        cipher = AES.new(KEY, AES.MODE_ECB)
+
+
+        # decrypt to get padded plaintext bytes 
+        padded = cipher.decrypt(ciphertext)
+
+        # remove PKCS#7 padding 
+        plaintext = pkcs7_unpad(padded, AES_BLOCK_SIZE)
+
+        # parse string for the pattern and return true if string exists
+        text = plaintext.decode("utf-8", errors="ignore")
+
+        return ";admin=true;" in text
+
+    # return false if string doesnt exist
+    except Exception:
+
+        return False
+    
+    
 
 
 
